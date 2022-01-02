a3c2f9aa6a8061cc9dae901b6271fce8b7f9070a
Meghan <hello@nektro.net>
2022-01-01 20:02:53 -0800

    build.zig- use deps.addAllTo

--- build.zig
+++ build.zig
@@ -5,15 +5,11 @@ pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const lib = b.addStaticLibrary("zig-double", "src/main.zig");
     lib.setBuildMode(mode);
-    for (deps.packages) |pkg| {
-        lib.addPackage(pkg);
-    }
+    deps.addAllTo(lib)
     lib.install();
 
     var main_tests = b.addTest("src/main.zig");
-    for (deps.packages) |pkg| {
-        main_tests.addPackage(pkg);
-    }
+    deps.addAllTo(main_tests);
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
